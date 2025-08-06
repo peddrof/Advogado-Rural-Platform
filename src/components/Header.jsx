4a28fba38282003d0c6a5801391c941b1564ac62
Peddro <168378429+peddrof@users.noreply.github.com>
2025-08-05 22:06:46 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = ({ scrolled }) => {
           >
             <img  
               alt="Logo Guilherme Medeiros Acessoria em Agronegocio"
-              src="https://iili.io/FFfeDcN.png"
+              src="https://i.ibb.co/GfLXTmxB/New-Project-50.png"
               className="h-10 md:h-14 w-auto"
             />
           </motion.div>
